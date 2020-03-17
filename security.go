8544b677ea7b762925e43da949270a5130de5949
nordmanii <thomas.nordmann@itslearning.com>
2020-03-17 22:37:25 +0100

    debug

--- security.go
+++ security.go
@@ -13,12 +13,12 @@ type security struct {
 	Wsse          string `xml:"wsse,attr"`
 	Wsu           string `xml:"wsu,attr"`
 	UsernameToken struct {
-		Username string `xml:"Username"`
+		Username string `xml:"wsse:Username"`
 		Password struct {
 			Text string `xml:",chardata"`
 			Type string `xml:"Type,attr"`
-		} `xml:"Password"`
-	} `xml:"UsernameToken"`
+		} `xml:"wsse:Password"`
+	} `xml:"wsse:UsernameToken"`
 }
 
 func SetSecurity(username, password string) *security {
@@ -38,3 +38,22 @@ func (p *security) ParseToXml() string {
 	}
 	return string(b)
 }
+
+/*
+<security wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+	<UsernameToken>
+		<Username>tnordmann</Username>
+		<Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">pat5rxe4</Password>
+	</UsernameToken>
+</security>
+*/
+
+/*
+
+   <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+       <wsse:UsernameToken>
+           <wsse:Username>cbcf5dc1-f410-4aca-9643-5a9cc5ecb17f</wsse:Username>
+           <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">e3NnmNVRU7iXvms</wsse:Password>
+       </wsse:UsernameToken>
+   </wsse:Security>
+*/
